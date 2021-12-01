6ecd0d1a1f11919b3d88aed1c24fda4233ecdc05
haoyu111 <72409343+haoyu111@users.noreply.github.com>
2021-12-01 10:25:49 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   
-  <h1 style="text-align:center;color:red;"> The tutor for vue.js!</h1>
-  <div>
-    <h2></h2>
+  <h1 style="text-align:center;color:blue;"> Ryerson University</h1>
+  <div class="box">
+    <h2>Introduction</h2>
+    <div id="intro">
+      <p>{{ message }}</p>
+    </div>
+
   </div>
 </template>
 
@@ -13,10 +17,14 @@ export default {
 </script>
 
 <style>
-div{
+.box{
   width:100%;
   border: 1px solid black;
 }
 
+var myObject = new Vue({
+    el: '#intro',
+    data: {message: 'Ryerson University (colloquially referred to as Ryerson, RyeU or RU) is a public research university located in Toronto, Ontario, Canada. The university's core campus is situated within the Garden District, although it also operates facilities elsewhere in Toronto. The university operates seven academic divisions/faculties, the Faculty of Arts, the Faculty of Communication and Design, the Faculty of Community Services, the Faculty of Engineering and Architectural Science, the Faculty of Science, the Lincoln Alexander School of Law, and the Ted Rogers School of Management. Many of these faculties are further organized into smaller departments and schools. The university also provides continuing education services through the G. Raymond Chang School of Continuing Education.'}
+})
 
 </style>
