85c287d59d228ec39870e5d437de8663dc374f66
haoyu111 <72409343+haoyu111@users.noreply.github.com>
2021-12-01 10:37:42 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -22,9 +22,5 @@ export default {
   border: 1px solid black;
 }
 
-myObject = new Vue({
-    el: '#intro',
-    data: {message: 'Ryerson University (colloquially referred to as Ryerson, RyeU or RU) is a public research university located in Toronto, Ontario, Canada. The universitys core campus is situated within the Garden District, although it also operates facilities elsewhere in Toronto. The university operates seven academic divisions/faculties, the Faculty of Arts, the Faculty of Communication and Design, the Faculty of Community Services, the Faculty of Engineering and Architectural Science, the Faculty of Science, the Lincoln Alexander School of Law, and the Ted Rogers School of Management. Many of these faculties are further organized into smaller departments and schools. The university also provides continuing education services through the G. Raymond Chang School of Continuing Education.'}
-    })
 
 </style>
